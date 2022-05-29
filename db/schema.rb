e210e169745c78d49b58be8fa3fce40aae38d6c7
Patricia <patricia.soto@routinguc.com>
2022-05-29 10:47:41 -0400

    add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_29_015151) do
+ActiveRecord::Schema.define(version: 2022_05_29_144556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2022_05_29_015151) do
     t.string "name"
     t.text "description"
     t.decimal "price", precision: 12, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "rut"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
