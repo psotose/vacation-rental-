04cb2e186bf5355ade7320d16cddecac967e0b39
Patricia <patricia.soto@routinguc.com>
2022-06-03 00:31:54 -0400

    add handling error

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,11 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        ContactMailer.notify(@contact).deliver_now
+        begin
+          ContactMailer.notify(@contact).deliver_now
+        rescue
+          format.html { redirect_to root_path, notice: 'En este momento no es posible enviar tu mensaje'}
+        end    
         format.html { redirect_to root_path, notice: 'Mensaje enviado exitosamente, nos pondremos en contacto contigo a la brevedad'} 
       else
         format.html { render :new }
